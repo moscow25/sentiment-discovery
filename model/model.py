Fixed -- model reloading; New default -- no Cell model w/ transform; WIP -- gram matrix for emotion text hidden state [only last so far, need to hack RNN model or run 60X times for average gram matrix

model/model.py
@@ -29,7 +29,7 @@ class RNNAutoEncoderModel(nn.Module):
                 tie_weights=True, freeze=False, teacher_force=True,
                 attention=False, init_transform_id=False,
                 use_latent_hidden=True, transform_latent_hidden=True,
-                use_cell_hidden=True, transform_cell_hidden=True):
+                use_cell_hidden=False, transform_cell_hidden=False):
         super(RNNAutoEncoderModel, self).__init__()
         self.freeze = freeze
         self.encoder = RNNModel(rnn_type=rnn_type, ntoken=ntoken, ninp=ninp, nhid=nhid,
@@ -104,20 +104,23 @@ class RNNAutoEncoderModel(nn.Module):
                 hval = self.emotion_hidden_state[n]
                 cval = self.emotion_cell_state[n]
                 if self.hidden_boost_factor != 0.0:
-                    v = hval * self.hidden_boost_factor
+                    v = hval
                     if self.average_cell_value:
-                        v = (v + encoder_hidden[0][0][0][n]) / 2.0
+                        v = (v * self.hidden_boost_factor) + encoder_hidden[0][0][0][n] * (1.0 - self.hidden_boost_factor)
+                        #v = (v + encoder_hidden[0][0][0][n]) / 2.0
+                    else:
+                        v = v * self.hidden_boost_factor
                     encoder_hidden[0][0][0][n] = v
                 if self.cell_boost_factor != 0.0:
                     encoder_hidden[1][0][0][n] = cval * self.cell_boost_factor
         # If we want to directly add to thes hidden state/cell state vector
         if self.use_added_hidden_state:
             print('Adding hidden state vector')
-            print(self.added_hidden_state_vector)
+            #print(self.added_hidden_state_vector)
             encoder_hidden[0][0][0] += self.added_hidden_state_vector
         if self.use_added_cell_state:
             print('Adding cell state vector')
-            print(self.added_cell_state_vector)
+            #print(self.added_cell_state_vector)
             encoder_hidden[1][0][0] += self.added_cell_state_vector
 
         emb = self.process_emb(encoder_hidden,
@@ -208,8 +211,10 @@ class RNNAutoEncoderModel(nn.Module):
         self.encoder.load_state_dict(sd['encoder'], strict)
         if not self.tied:
             self.decoder.load_state_dict(sd['decoder'], strict)
-        self.latent_hidden_transform.load_state_dict(sd['hidden_transform'], strict)
-        self.latent_cell_transform.load_state_dict(sd['cell_transform'], strict)
+        if sd['hidden_transform']:
+            self.latent_hidden_transform.load_state_dict(sd['hidden_transform'], strict)
+        if sd['cell_transform']:
+            self.latent_cell_transform.load_state_dict(sd['cell_transform'], strict)
 
 # Placeholder QRNN wrapper -- to support detach/reset/init RNN state
 #class myQRNN(QRNN):

transfer.py
@@ -78,7 +78,10 @@ with open(args.load_model, 'rb') as f:
     print(sd.keys())
     # Hack -- extra transform.
     extra_transform = nn.Linear(args.nhid, args.nhid).cuda()
-    extra_transform.load_state_dict(sd['cell_transform'], strict=True)
+    if sd['cell_transform']:
+        extra_transform.load_state_dict(sd['cell_transform'], strict=True)
+    else:
+        print('WARNING -- no extra cell-cell transform')
     if 'encoder' in sd:
         sd = sd['encoder']
 
@@ -169,9 +172,9 @@ def transform(model, text):
         labels = (np.concatenate(labels).flatten())
     print('%0.3f seconds to transform %d examples' %
                   (time.time() - tstart, n))
-    f = features
+#    f = features
 #    f = features_xform
-#    f = features_hidden
+    f = features_hidden
 
     # For fun, also show the vector of POS - NEG == simply from the means of these distributions
     pos_sum = np.sum(labels)
@@ -359,6 +362,7 @@ for idx in top_pos:
    topAve[idx] = trAve[idx]
 print(np.argsort(topAve))
 print(topAve[np.argsort(topAve)])
+# Never mind -- just use the whole vector. Hard to know what top activations or sentiment neurons mean
 topAve = trAve
 
 # Save the vector -- which can be applied directly on Negative/Neutral hidden state to make it more positive?